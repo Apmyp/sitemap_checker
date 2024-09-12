0e47522312837bd161237b038605d3add44c2537
Arthur Bordenyuk <arthur.bordenyuk@gmail.com>
2024-09-12 10:54:56 +0300

    Combines everything up to a working prototype

--- sitemap_checker.go
+++ sitemap_checker.go
@@ -19,14 +19,15 @@ const (
 )
 
 func main() {
-	sitemaps := []string{"asd"}
-
 	ctx, cancel := context.WithCancel(context.Background())
 	ctx = context.WithValue(ctx, CONC_KEY, getConcArgument())
 	defer cancel()
 
+	sitemaps := []string{"https://www.mixbook.com/sitemaps/sitemap_index.cached.xml.gz"}
 	smCh := processSitemaps(ctx, sitemaps)
 	checkUrls(ctx, smCh)
+
+	os.Exit(0)
 }
 
 func getConcArgument() int {
@@ -43,34 +44,6 @@ func getConcArgument() int {
 	return number
 }
 
-func checkUrls(ctx context.Context, in <-chan string) {
-	var wg sync.WaitGroup
-	nConc := ctx.Value(CONC_KEY).(int)
-	wg.Add(nConc)
-
-	work := func() {
-		defer wg.Done()
-		for url := range in {
-			select {
-			case <-ctx.Done():
-				return
-			default:
-				if err := isUrlAlive(url); err != nil {
-					log.Printf("DEAD URL [%s]: %s", err, url)
-				} else {
-					log.Printf("ALIVE URL: %s", url)
-				}
-			}
-		}
-	}
-
-	for range nConc {
-		go work()
-	}
-
-	wg.Wait()
-}
-
 func processSitemaps(ctx context.Context, sitemaps []string) <-chan string {
 	out := make(chan string, ctx.Value(CONC_KEY).(int))
 	go func() {
@@ -78,10 +51,14 @@ func processSitemaps(ctx context.Context, sitemaps []string) <-chan string {
 		var wg sync.WaitGroup
 		wg.Add(len(sitemaps))
 		for _, sm := range sitemaps {
-			go func() {
-				defer wg.Done()
-				parseSitemap(ctx, sm, out)
-			}()
+			select {
+			case <-ctx.Done():
+			default:
+				go func() {
+					defer wg.Done()
+					parseSitemap(ctx, sm, out)
+				}()
+			}
 		}
 		wg.Wait()
 	}()
@@ -94,26 +71,6 @@ type SitemapUrl struct {
 
 type SitemapUrls struct {
 	Urls []SitemapUrl `xml:"url"`
-}
-
-func fetchSitemap(sm string) ([]byte, error) {
-	return []byte(`<urlset><url><loc>https://www.mixbook.com/mother-s-day-photo-books</loc></url><url><loc>https://www.mixbook.com/father-s-day-photo-books</loc></url></urlset>`), nil
-	var client http.Client
-	req, err := http.NewRequest("GET", sm, nil)
-	if err != nil {
-		return []byte{}, err
-	}
-	req.Header.Add("User-Agent", USER_AGENT)
-	res, err := client.Do(req)
-	if err != nil {
-		return []byte{}, err
-	}
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		return []byte{}, err
-	}
-
-	return body, nil
 }
 
 func parseSitemap(ctx context.Context, sm string, out chan<- string) {
@@ -138,8 +95,29 @@ func parseSitemap(ctx context.Context, sm string, out chan<- string) {
 	}
 }
 
+func fetchSitemap(sm string) ([]byte, error) {
+	// return []byte(`<urlset><url><loc>https://www.mixbook.com/mother-s-day-photo-books</loc></url><url><loc>https://www.mixbook.com/father-s-day-photo-books</loc></url></urlset>`), nil
+	log.Printf("Fetching sitemap: %s", sm)
+	var client http.Client
+	req, err := http.NewRequest("GET", sm, nil)
+	if err != nil {
+		return []byte{}, err
+	}
+	req.Header.Add("User-Agent", USER_AGENT)
+	res, err := client.Do(req)
+	if err != nil {
+		return []byte{}, err
+	}
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return body, nil
+}
+
 func isUrlAlive(url string) error {
-	return nil
+	// return nil
 	var client http.Client
 	req, err := http.NewRequest("HEAD", url, nil)
 	if err != nil {
@@ -158,4 +136,33 @@ func isUrlAlive(url string) error {
 	} else {
 		return fmt.Errorf("status_code=%d", status)
 	}
+}
+
+func checkUrls(ctx context.Context, in <-chan string) {
+	var wg sync.WaitGroup
+	nConc := ctx.Value(CONC_KEY).(int)
+	wg.Add(nConc)
+
+	work := func() {
+		defer wg.Done()
+		for url := range in {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				if err := isUrlAlive(url); err != nil {
+					log.Fatalf("DEAD URL [%s]: %s", err, url)
+					os.Exit(1)
+				} else {
+					log.Printf("ALIVE URL: %s", url)
+				}
+			}
+		}
+	}
+
+	for range nConc {
+		go work()
+	}
+
+	wg.Wait()
 }
